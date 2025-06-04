c18f15b12d3a8cda7e38c28e8e7599cc907c27bd
Thuani Sampaio <thuanisc@gmail.com>
2025-06-04 20:39:45 -0300

    second commit

--- PythonSQL.sql
+++ PythonSQL.sql
@@ -5,3 +5,10 @@ USE PythonSQL;
 SHOW TABLES;
 
 DESCRIBE usuarios;
+
+SELECT * FROM usuarios;
+
+SELECT u.nome, l.titulo, e.data_emprestimo
+FROM usuarios u
+JOIN emprestimos e ON u.id = e.usuario_id
+JOIN livros l ON l.id = e.livro_id;
